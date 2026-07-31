6588c05ca42b1094ca46e1c907c7d170d4eb903b
ashumali0611-png <ashumali0611@gmail.com>
2026-07-31 09:32:25 +0530

    added one more line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -2,9 +2,10 @@
 public class WelcomeToJava {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Java Track");
-        System.out.println("I already know how to build Java programming");
-        System.out.println("I want to become a Java Developer");
-        System.out.println("Now i know how to use git and github");
+        System.out.println("Welcome to the Java Track.");
+        System.out.println("I already know how to build programming logic.");
+        System.out.println("Now I am learning how to express that logic using Java.");
+        System.out.println("My goal is to become a Java Developer.");
+        System.out.println("I selected Java because I want to build large applications.");
     }
 }
