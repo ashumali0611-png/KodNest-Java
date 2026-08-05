93c26ad9939fe255a1232d59557e5ea0e512233d
ashumali0611-png <ashumali0611@gmail.com>
2026-08-05 12:22:41 +0530

    Forloop

---
+++ MOD02/roleofbreak.java
@@ -0,0 +1,16 @@
+
+public class roleofbreak {
+
+    public static void main(String[] args) {
+        int choice = 1;
+
+        switch (choice) {
+            case 1:
+                System.out.println("Java");
+            case 2:
+                System.out.println("SQL");
+        }
+
+    }
+
+}
