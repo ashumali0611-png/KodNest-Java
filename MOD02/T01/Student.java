7912cde2bc7e1ba123094148cb63b5b80b19e7ab
ashumali0611-png <ashumali0611@gmail.com>
2026-08-12 12:30:19 +0530

    Shadowing

---
+++ MOD02/T01/Student.java
@@ -0,0 +1,26 @@
+
+class Student {
+
+    String name;
+    int age;
+    double height;
+
+    void input(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.input("Ashwini", 21, 5.6);
+        s1.display();
+    }
+
+}
