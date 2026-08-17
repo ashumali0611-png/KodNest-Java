0e1f8d95220be2131d85215542361829b12319b9
ashumali0611-png <ashumali0611@gmail.com>
2026-08-17 12:02:00 +0530

    Array

---
+++ track/Array.java/Pgm2.java
@@ -0,0 +1,12 @@
+
+public class Pgm2 {
+
+    public static void main(String[] args) {
+        int a[] = {11, 22, 31, 40, 60};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+    }
+}
