ac9c30278d693891e7c53b00f55f6b72d82b797d
ashumali0611-png <ashumali0611@gmail.com>
2026-08-17 13:04:01 +0530

    Array

---
+++ track/Array.java/Pgm4.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Pgm4 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a[] = new int[5];
+        System.out.println("Enter Array Elements: ");
+        for (int i = 0; i <= a.length - 1; i++) {
+            a[i] = scan.nextInt();
+        }
+        System.out.println("Array Elements Are: ");
+        for (int i = a.length - 1; i >= 0; i--) {
+            System.out.println(a[i]);
+        }
+        scan.close();
+    }
+}
